08074ad841a37f3bf1b3a7f62891f69f8d12db27
Dina007 <dina.rajaomazava@yahoo.com>
2018-08-31 10:34:56 +0200

    Methode Trier Par Ordre Alphabétique

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -1,35 +1,22 @@
 package com.ecommerce.microcommerce.web.controller;
-
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.MappingJacksonValue;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
 import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 @Api( description="API pour es opérations CRUD sur les produits.")
 
@@ -45,7 +32,6 @@ public class ProductController {
     //Récupérer la liste des produits
 
     @RequestMapping(value = "/Produits", method = RequestMethod.GET)
-
     public MappingJacksonValue listeProduits() {
 
         Iterable<Product> produits = productDao.findAll();
@@ -65,7 +51,6 @@ public class ProductController {
     //Récupérer un produit par son Id
     @ApiOperation(value = "Récupère un produit grâce à son ID à condition que celui-ci soit en stock!")
     @GetMapping(value = "/Produits/{id}")
-
     public Product afficherUnProduit(@PathVariable int id) {
 
         Product produit = productDao.findById(id);
@@ -74,8 +59,6 @@ public class ProductController {
 
         return produit;
     }
-
-
 
 
     //ajouter un produit
@@ -121,37 +104,31 @@ public class ProductController {
 
     //calculer la marge d'un produit
     @RequestMapping(value="/AdminProduits", method = RequestMethod.GET)
-
     public List<String> calculerMargeProduit(){
 
         	
-        Iterable<Product> produits = productDao.findAll();
+        List<Product> produits = productDao.findAll();
         List<String> produitAfficher = new ArrayList<>();
-        
-        for (Product product1 :produits) {
 
-        	int marge = product.getPrix() - product.getPrixAchat();
+        for (Product product1 :produits) {
+        	int marge = product1.getPrix()-product1.getPrixAchat();
         	produitAfficher.add(product1.toString()+":"+marge);
         }
 
         return produitAfficher;
     }
     
-    /*
- // Calculer la marge des produits
-    @RequestMapping(value = "/AdminProduits", method = RequestMethod.GET)
-    public List<String> calculerMargeProduit() {
-
-        Iterable<Product> produits = productDao.findAll();
-        List<String> produitsMarge = new ArrayList<>();
-
-        for (Product produit : produits) {
-            int marge = produit.getPrix() - produit.getPrixAchat();
-            produitsMarge.add(produit.toString() + ": " + marge);
-        }
-
-        return produitsMarge;
-    }
-     */
     
+    //tri des produits par ordre alphabétique
+    @GetMapping(value="/TrierProduits")
+
+    public List<Product> trierProduitsParOrdreAlphabetique(){
+
+    	List<Product> produitsTrier = productDao.findByOrderByNom();
+
+        return produitsTrier;
+    }
+    
+    
+
 }
